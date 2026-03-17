65f1e9349554a1d3ba30527d46537352473ddb5f
Niel <dyprih@gmail.com>
2026-03-17 19:14:04 +0100

    remove 'ref' argument that is not used

--- pid.py
+++ pid.py
@@ -1,6 +1,5 @@
 class PID:
-    def __init__(self, ref, Kp, Ki=0, Kd=0):
-        self.ref = ref
+    def __init__(self,Kp, Ki=0, Kd=0):
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
