e6f91c68f7d79a11bb34d67ac91b5cb342f24888
Niel <dyprih@gmail.com>
2026-03-17 18:51:41 +0100

    Finalize PID regulator, add 'reset' method

--- pid.py
+++ pid.py
@@ -1,9 +1,21 @@
 class PID:
-    def __init__(self, Kp, Ki=0, Kd=0):
+    def __init__(self, ref, Kp, Ki=0, Kd=0):
+        self.ref = ref
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
+        self.sum = 0
+        self.prev = 0
 
     def regulate(self, error, dt):
-        res = error * self.Kp
+        self.sum += error * dt
+        P = error * self.Kp
+        I = self.sum * self.Ki
+        D = (error - self.prev) / dt * self.Kd
+        res = P + I + D
+        self.prev = error
         return res
+
+    def reset(self):
+        self.sum = 0
+        self.prev = 0
